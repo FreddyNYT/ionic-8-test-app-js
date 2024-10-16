6b0f0a071891c42d3a98f1809054a76a3a30751b
ophalvens <ophalvens@gmail.com>
2024-10-16 08:23:53 +0200

    demo reactiviteit en event-hooks in Ionic

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -3,18 +3,18 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
+        <ion-tab-button tab="tab1" href="/tabs/tabnieuw">
+          <ion-icon aria-hidden="true" :icon="addCircle" />
           <ion-label>Tab 1</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
+        <ion-tab-button tab="tab2" href="/tabs/tabOverzicht">
+          <ion-icon aria-hidden="true" :icon="listOutline" />
           <ion-label>Tab 2</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
+        <ion-tab-button tab="tab3" href="/tabs/tabExperiment">
+          <ion-icon aria-hidden="true" :icon="constructSharp" />
           <ion-label>Tab 3</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
@@ -24,5 +24,15 @@
 
 <script setup>
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { addCircle, listOutline, constructSharp } from 'ionicons/icons';
+
+// In dit voorbeeld zijn icons gebruikt uit de 3 Ionicons sets:
+//  standaard of filled (addCircle)
+//  Outline (listOutline)
+//  Sharp (constructSharp)
+// Voor consistentie blijf je in je eigen project best in 1 set.
+// Let op : de syntax op https://ionic.io/ionicons/ volgt een ander patroon ("-" concatenatie)
+//  add-circle online -> addCircle in je project
+//  list-outline -> listOutline in je project
+//  construct-sharp -> constructSharp in je project
 </script>
